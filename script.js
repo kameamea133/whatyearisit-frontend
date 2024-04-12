95091a211bb66886e661bfeb267a5e30c74857cf
kameamea133 <monnier1977@gmail.com>
2024-04-12 17:15:58 +0200

    test101

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 fetch('https://whatyearisit-backend-bice-omega.vercel.app/year')
  .then(response => response.json())
  .then((data)=> {
-  document.querySelector('#year').textContent = `this is the year: ${data.toString()}`
+  document.querySelector('#year').textContent = `this is the year: ${data.getFullYear().toString()}`
  })
